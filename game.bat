ed2f2b65058c534e5b178a1407416a6fe73f0bfb
BlueMonkey26 <eli@gessaman.com>
2025-06-30 17:48:10 -0700

    a

--- game.bat
+++ game.bat
@@ -1,31 +1,18 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 
-set /a target=%random% %% 100 + 1
-set tries=0
+echo Guess the secret color!
+echo Options: red, blue, green
+set secret=blue
 
 :guess
-set /p guess=Guess a number between 1 and 100:
+set /p guess=Enter your guess:
 
-:: Validate input is a number
-echo %guess%| findstr /r "^[0-9][0-9]*$" >nul
-if errorlevel 1 (
-    echo Please enter a valid number.
-    goto guess
-)
-
-set /a tries+=1
-
-:: Now numeric compare
-if %guess%==%target% (
-    echo Correct! You guessed it in %tries% tries.
+if /i "%guess%"=="%secret%" (
+    echo Correct! You guessed the secret color.
     goto end
 ) else (
-    if %guess% lss %target% (
-        echo Too low, try again.
-    ) else (
-        echo Too high, try again.
-    )
+    echo Nope, try again.
     goto guess
 )
 
