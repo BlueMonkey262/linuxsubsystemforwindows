6c753280b4796e4cbdf78dc74d52f9754dc33e35
BlueMonkey26 <eli@gessaman.com>
2025-06-30 17:46:55 -0700

    a

--- game.bat
+++ game.bat
@@ -6,13 +6,22 @@ set tries=0
 
 :guess
 set /p guess=Guess a number between 1 and 100:
+
+:: Validate input is a number
+echo %guess%| findstr /r "^[0-9][0-9]*$" >nul
+if errorlevel 1 (
+    echo Please enter a valid number.
+    goto guess
+)
+
 set /a tries+=1
 
-if "!guess!"=="%target%" (
-    echo Correct! You guessed it in !tries! tries.
+:: Now numeric compare
+if %guess%==%target% (
+    echo Correct! You guessed it in %tries% tries.
     goto end
 ) else (
-    if !guess! lss %target% (
+    if %guess% lss %target% (
         echo Too low, try again.
     ) else (
         echo Too high, try again.
